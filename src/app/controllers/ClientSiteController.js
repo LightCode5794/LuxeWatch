5ae5f7574b8ee63a1532e231564011ee6838db67
LightCode5794 <trantronghoang5794@gmail.com>
2023-05-16 08:42:54 +0700

    update UI home

--- src/app/controllers/ClientSiteController.js
+++ src/app/controllers/ClientSiteController.js
@@ -1,11 +1,36 @@
 const { singleMongooseToObject } = require('../../util/mongoose');
+const { multipleMongooseToObject } = require('../../util/mongoose');
+const Product = require('../models/product/product.model');
+const Category = require('../models/category/category.model');
+const Brand = require('../models/brand/brand.model');
+const Tag = require('../models/tag/tag.model');
 
 class ClientSiteController {
     //[GET] /admin/dashboard
-    home(req, res, next) {
-       // res.send('home page')
-        // res.json(req.params)
-        res.render('home');
+    async home(req, res, next) {
+        try {
+            const products = await Product.find()
+                .populate('brand')
+                .populate('category')
+                .populate('tags');
+            const brands = await Brand.find();
+
+            res.render('home', {
+                brands: multipleMongooseToObject(brands),
+                products: multipleMongooseToObject(products),
+            });
+
+            // .then((products) => {
+            //     res.render('home', {
+            //         products: multipleMongooseToObject(products),
+            //     });
+
+            // })
+            // .catch(next);
+        } catch (err) {
+            res.status(401).send(err.message);
+        }
+
     }
 }
 
