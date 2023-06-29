9228db629cfbfb4f53d936d7f699c129f06af719
LightCode5794 <90496996+LightCode5794@users.noreply.github.com>
2023-06-30 05:34:56 +0700

    update Authorization

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,11 @@
 const isLoggedIn = (req, res, next) => {
     req.user ? next() : res.sendStatus(401);
 };
+const isAdmin = (req, res, next) => {
+    req.user && req.user.email == process.env.EMAIL_ADMIN ? next() : res.sendStatus(401);
+}
+;
 module.exports = {
     isLoggedIn,
+    isAdmin,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ const authRouter = require('./auth');
 const categoriesRouter = require('./categories');
 const adminRoutes = require('./admin');
 const clientRoutes = require('./client');
-const { isLoggedIn } = require('../middlewares/auth');
+const { isLoggedIn, isAdmin } = require('../middlewares/auth');
+
 
 function routes(app) {
-    app.use('/admin',  adminRoutes);
+    app.use('/admin', isAdmin, adminRoutes);
     app.use('/auth', authRouter);
     app.use('/', clientRoutes);
 }
